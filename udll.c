c598814f271d09bcdf1fb7603e4b0fc2a6ad54bc
Marshall Ruse <12mr9@queensu.ca>
2016-12-01 15:46:37 -0500

    Added iterate from end functionality

--- udll.c
+++ udll.c
@@ -9,11 +9,13 @@ Node* getNewNode(union Data data) {
 	newNode->next = NULL;
 	return newNode;
 }
-void insert(int index, union Data data){ //need to be finished
+
+void insert(int index, union Data data){
 	Node* newNode = getNewNode(data);
 
 	if (head == NULL){ //empty list
 		head = newNode;
+		last = newNode;
 		head->length = 1;
 		return;
 	}
@@ -25,66 +27,89 @@ void insert(int index, union Data data){ //need to be finished
 		head->length = head.length + 1;
 
 	}
-
-	else if(index == head.length - 1 ){ //add to the end
-		Node* temp = head;
-		while(temp->next != NULL){
-			temp = temp->next;
-		}
+	else if (index == head.length - 1){
+		Node* temp = last;
+		last = newNode;
+		last->previous = temp;
+		last->next = NULL;
+		head->length = head.length +1;
+	}
+	else if (index <= (head.length - 1) - 2) {//add to middle
+		if (index <= (head.length / 2)){ //closer to beginning
+			Node* temp = head;
+			Node* temp1;
+			int i = 0;
+			while (i< index -1){
+				temp = temp->next;
+				i++;
+			}
+			temp1 = temp->next;
 			temp->next = newNode;
 			newNode->previous = temp;
-			head->length = head.length + 1;
-	}
-	else if (index <= head.length) {//add to middle
-		Node* temp = head;
-		Node* temp1;
-		int i = 0;
-		while (i< index -1){
-			temp = temp->next;
-			i++;
+			newNode->next = temp1;
+			if (temp1 != NULL){
+				temp1->previous = newNode;
+			}
 		}
-		temp1 = temp->next;
-		temp->next = newNode;
-		newNode->previous = temp;
-		newNode->next = temp1;
-		if (temp1 != NULL){
-			temp1->previous = newNode;
+		else if(index > ((head.length - 1) / 2)){ //closer to end
+			Node* temp = last;
+			Node* temp1;
+			int i = head.length;
+			while (i > index){
+				temp = temp->previous;
+				i--;
+			}
+			temp1 = temp->previous;
+			temp->previous = newNode;
+			newNode->previous = temp1;
+			newNode->next = temp;
+			temp1->next = newNode;
 		}
+
 		head->length = head.length + 1;
-
 	}
 	}
 
-}
-
-void removes(int index){ //need to be finished
-	Node* temp = head;
+void removes(int index){
 	if (head == NULL){// empty list
 		printf("List is empty. Nothing to delete.\n");
-	}
-	int i;
-	for(i=0; i<index && temp!=NULL; i++)
-	{
-		temp = temp->next;
+		return;
 	}
 
-	if(index == 0)//delete from beginning
-	{
+	if(index == 0){//delete from beginning
 		head = head->next;
 		head->previous = NULL;
 	}
-	else if(index == last)//delete from end
-	{
+	else if(index == head.length -1){//delete from end
 		last = last->previous;
-		last->next = NULL;		
+		last->next = NULL;
 	}
-	else if(temp != NULL)
-	{
-	temp->previous->next = temp->next;
-	temp->next->previous = temp->previous;
+	else if (index <= ((head.length -1) / 2)){ //closer to beginning
+		Node* temp = head;
+		Node* before;
+		Node* after;
+		for(int i = 0; i < index; i++){
+			temp = temp->next;
+		}
+		before = temp->previous;
+		after = temp->next;
+		before->next = temp->next;
+		after->previous = temp->previous;
+		free(temp);
 	}
-	free(temp)
-
+	else if (index > ((head.length - 1) / 2)){ //closer to end
+		Node* temp = last;
+		Node* before;
+		Node* after;
+		for(int i = head.length - 1; i > index; i--){
+			temp = temp->previous;
+		}
+		before = temp->previous;
+		after = temp->next;
+		before->next = temp->next;
+		after->previous = temp->previous;
+		free(temp);
+	}
 }
 
 int length()
