695a690e2e4cde17571410a43bc3bb4b54951381
Marshall Ruse <12mr9@queensu.ca>
2016-12-01 13:06:57 -0500

    changed length functionality

--- udll.c
+++ udll.c
@@ -14,19 +14,26 @@ void insert(int index, union Data data){ //need to be finished
 
 	if (head == NULL){ //empty list
 		head = newNode;
+		head->length = 1;
 		return;
 	}
 	else if (index == 0) { //add to the beginning
 		head->previous = newNode;
 		newNode->next = head;
+		newNode->length = head.length;
 		head = newNode;
+		head->length = head.length + 1;
+
 	}
 
 	else if(index == head.length - 1 ){ //add to the end
 		Node* temp = head;
-		while(temp->next != NULL) temp = temp->next;
+		while(temp->next != NULL){
+			temp = temp->next;
+		}
 			temp->next = newNode;
 			newNode->previous = temp;
+			head->length = head.length + 1;
 	}
 	else if (index <= head.length) {//add to middle
 		Node* temp = head;
@@ -43,7 +50,7 @@ void insert(int index, union Data data){ //need to be finished
 		if (temp1 != NULL){
 			temp1->previous = newNode;
 		}
-		head->length= length();
+		head->length = head.length + 1;
 
 	}
 	}
@@ -59,7 +66,10 @@ void removes(int index){ //need to be finished
 	else {
 		if (deleteNode == head){ //delete node is the head node
 			temp = temp->next;
+			temp->length = head->length;
 			temp->previous= NULL;
+			head = temp;
+			head->length = head.length - 1;
 		}
 		else {// delete node is not the head node
 			if(deleteNode->next != NULL){// change next
@@ -68,6 +78,7 @@ void removes(int index){ //need to be finished
 			if(deleteNode->previous != NULL){//change previous
 				deleteNode->previous->next = deleteNode->next;
 			}
+		head->length = head.length -1;
 		}
 	}
 	free(deleteNode);
@@ -77,7 +88,7 @@ void removes(int index){ //need to be finished
 
 int length()
 {
-	return head.data.i
+	return head.length;
 }
 
 union Data get(int index){
