e855aed0a1826f6ad05a8f50716b09ded66e347d
archiveteng <tengsitong@hotmail.com>
2016-12-01 23:53:27 -0500

    Add files via upload
    mark this one!!!!

--- main.c
+++ main.c
@@ -4,11 +4,73 @@
  *Marshall Ruse 10066247 
  */
 
-#include<stdio.h>
-#include"udll.h"
 int main() {
-  printf("This function is used to test universal doubly linked list."\n);
-  
-  return 0;
+	union Data one, two, three, four, ptr;
+	one.charData = 'a';
+	two.charData = 'b';
+	three.charData = 'c';
+	four.intData = -10;
 
+	float floatData = 1.23456;
+	ptr.floatPtrData = &floatData;
+
+	printf("print the list:\n");
+	printList();
+	printf("  List Length: %d\n\n", length());
+
+	insert(0, one, CHARDATA);
+	printList();
+	printf("  List Length: %d\n\n", length());
+
+	insert(1, two, CHARDATA);
+	printList();
+	printf("  List Length: %d\n\n", length());
+
+	insert(1, three, CHARDATA);
+	printList();
+	printf("  List Length: %d\n\n", length());
+
+	insert(3, four, INTDATA);
+	printList();
+	printf("  List Length: %d\n\n", length());
+
+	removeNode(1);
+	printList();
+	printf("  List Length: %d\n\n", length());
+
+	insert(0, one, CHARDATA);
+	printList();
+	printf("  List Length: %d\n\n", length());
+
+	removeNode(2);
+	printList();
+	printf("  List Length: %d\n\n", length());
+
+	insert(3, ptr, FLOATPTRDATA);
+	printList();
+	printf("  List Length: %d\n\n", length());
+
+	insert(1, two, CHARDATA);
+	printList();
+	printf("  List Length: %d\n\n", length());
+
+	insert(4, one, CHARDATA);
+	printList();
+	printf("  List Length: %d\n\n", length());
+
+	char character;
+	int number;
+
+
+	printf("Fetching data from node 1 ...\n");
+	character = get(1).charData;
+	printf("we got %c\n\n", character);
+
+
+
+	printf("Fetching data from node 4 ...\n");
+	number = get(4).intData;
+	printf("we got %d\n\n", number);
+
+	printf("\n");
 }
